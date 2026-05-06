d2072d93bb544e7e1b60c6e2ca4b4bdd3be705ea
Vansh Malik <vanshmalik303@gmail.com>
2026-05-06 22:56:13 +0530

    up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './components/landing/LandingPage'
 import EventsListPage from './pages/EventsListPage'
 import EventDetailPage from './pages/EventDetailPage'
+import About from './pages/About'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<About />} />
+
         <Route path="/events" element={<EventsListPage />} />
         <Route path="/events/:slug" element={<EventDetailPage />} />
       </Routes>
