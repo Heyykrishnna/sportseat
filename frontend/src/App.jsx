05b1304d715b276455fba7b925bfecd489ba8871
Yatharth <khandelwalyatharth39@gmail.com>
2026-05-07 14:53:25 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './components/landing/LandingPage'
 import EventsListPage from './pages/EventsListPage'
 import EventDetailPage from './pages/EventDetailPage'
-import NavEvents from './pages/Navicons/NavEvents'
 import Mytickets from './pages/Navicons/Mytickets'
 
 function App() {
@@ -10,11 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-
-        
-        <Route path="/events" element={<NavEvents />} />
-        <Route path="/mytickets" element={<Mytickets />} />
-
+                <Route path="/mytickets" element={<Mytickets />} />
 
 
         <Route path="/events" element={<EventsListPage />} />
